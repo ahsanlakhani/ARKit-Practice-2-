36d5e707535de19efcf14425a9514f040fcd921a
Ahsan Lakhani <30596013+ahsanlakhani@users.noreply.github.com>
2017-10-17 14:33:22 +0500

    UIBezierPath Drawing

--- World Tracking/DefaultShapesViewController.swift
+++ World Tracking/DefaultShapesViewController.swift
@@ -1,15 +1,15 @@
 //
-//  RandomPositionObjectViewController.swift
+//  DefaultShapesViewController.swift
 //  World Tracking
 //
-//  Created by admin on 14/10/2017.
+//  Created by admin on 16/10/2017.
 //  Copyright © 2017 admin. All rights reserved.
 //
 
 import UIKit
 import ARKit
 
-class RandomPositionObjectViewController: UIViewController {
+class DefaultShapesViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
     
@@ -35,33 +35,72 @@ class RandomPositionObjectViewController: UIViewController {
         //a node is simply a position in space/sceneView it has no shape no size no color
         let node = SCNNode()
         
+        //cap radius is how thick the capsule is
+//        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+        
+        //if topRadius = 0 so the cone will be pointed from top
+        //node.geometry = SCNCone(topRadius: 0, bottomRadius: 0.1, height: 0.2)
+        
+        //to make cylinder from cone
+//        node.geometry = SCNCone(topRadius: 0.1, bottomRadius: 0.1, height: 0.2)
+        
+        //for cylinder
+//        node.geometry = SCNCylinder(radius: 0.1, height: 0.2)
+        
+        //for sphere
+//        node.geometry = SCNSphere(radius: 0.1)
+        
+        //for tube
+//        node.geometry = SCNTube(innerRadius: 0.1, outerRadius: 0.2, height: 0.3)
+        
+        //for torus/ring
+        //will use this shape to make basketball hoop/ring
+//        node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+        
+        //to make a plane
+//        node.geometry = SCNPlane(width: 0.1, height: 0.1)
+
+        //pyramid
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        
+        //bezier path(custom shapes)
+        let path = UIBezierPath()
+        //we are drawing a line from (0,0) to (0,0.3)
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: 0.2))
+        //drawing a diagonal line from (0,0.3) to (0.6,0.6)
+        //we will draw a house after running this code
+        path.addLine(to: CGPoint(x: 0.2, y: 0.4))
+        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.4, y: 0))
+        
+        //extrusion depth is the thickness of line
+        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+        node.geometry = shape
+        
         //node has no attributes so we have to give it those attributes
         //first we give it a shape
         //node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         
         //for rounded edges
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.02)
+        //node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.02)
         
-        //for sphere
+        //for making sphere with box
         //node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.1/2)
         
         //giving the geometry a specular light
         //specular is a light that reflected off away the surface
-        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        //node.geometry?.firstMaterial?.specular.contents = UIColor.white
         
         //first material defines the appearance of the surface of our node which is box in ourt matter
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
-        
-        //generating random numbers
-        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+    
         
         //scnvector3 is a 3 dimensional vector
         //0,0,0 means the box will be at the world origin
         //node.position = SCNVector3(0,0,0)
         //z=-0.3 means 0.3 meters away from z axis
-        node.position = SCNVector3(x,y,z)
+        node.position = SCNVector3(0,0,-0.7)
         
         //add the node to the scene view
         //the scene is what's displaying the camera view of the real world
@@ -97,5 +136,6 @@ class RandomPositionObjectViewController: UIViewController {
     func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
         return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
     }
+
 
 }
